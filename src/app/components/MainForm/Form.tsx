b5f4e2949fc5924468ae9a30eceff5e77bcdaa7b
Renat <renatovrenat17@mail.ru>
2024-08-07 20:44:34 +0300

    config commit

--- src/app/components/MainForm/Form.tsx
+++ src/app/components/MainForm/Form.tsx
@@ -19,25 +19,24 @@ export const MainForm = () => {
       const {register,reset,handleSubmit,formState:{errors}} = useForm<I_Form>({
         mode:'onBlur'
       })
-     const onSubmit: FormEventHandler<HTMLFormElement> = async(data) => {
-      data.preventDefault()
-      const formData = new FormData(data.currentTarget)
-      const res = await signIn('credentials',{
-        email:formData.get('email'),
-        password:formData.get('password'),
+     const onSubmit:SubmitHandler<I_Form> = async(data) => {
+      const res =  await signIn('credentials',{
+        email:data.email,
+        password:data.password,
         redirect:false
       });
       if (res && !res.error) {
         router.push('/profile')
       } else {
         console.log(res);
+        
       }
       reset()
      }  
       
   return (
     <>
-    <form className=" max-w-[270px] w-full  flex flex-col justify-center items-center gap-4 mt-[40px] " action='' onSubmit={(onSubmit)}>
+    <form className=" max-w-[270px] w-full  flex flex-col justify-center items-center gap-4 mt-[40px] " action='' onSubmit={handleSubmit(onSubmit)}>
     <div className="max-h-14 w-full" >
       <input type="text" {...register('email',{
         required:true,
